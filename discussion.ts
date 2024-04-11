b56104288f89b2bde73a7e4e573d93e244719a73
sendist <sendisetiawan10902@gmail.com>
2024-04-11 19:23:48 +0700

    implement verify comment

--- discussion.ts
+++ discussion.ts
@@ -6,6 +6,7 @@ const router = express.Router();
 router.post("/thread", async (req, res) => {
   const { data } = await req.body;
   console.log(req.body);
+  console.log(data);
 });
 
 // definisikan semua subroute disini
@@ -59,6 +60,31 @@ router.get("/:id", async (req, res) => {
   res.json(discussion);
 });
 
+router.delete("/:id", async (req, res) => {
+  // #swagger.tags = ['Discussion']
+  // #swagger.description = 'Delete a discussion by id'
+  const id = parseInt(req.params.id);
+  const userId = req.body.userId;
+
+  const creator = await prisma.thread.findUnique({
+    where: { id },
+    select: { user_id: true },
+  });
+
+  if (!creator) {
+    return res.status(404).json({ error: "Discussion not found" });
+  } else
+
+  if (creator.user_id !== userId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  } else {
+    await prisma.thread.delete({
+      where: { id },
+    });
+    res.json({ message: "Discussion deleted" });
+  }
+});
+
 router.patch("/comment/:id/upvote", async (req, res) => {
   // #swagger.tags = ['Discussion']
   // #swagger.description = 'Get a comment by id'
@@ -98,7 +124,7 @@ router.patch("/comment/:id/upvote", async (req, res) => {
           data: { comment_id: id, user_id: userId },
         }),
       ]);
-      res.json({ message: "Upvote added", voted: true});
+      res.json({ message: "Upvote added", voted: true });
     }
   } catch (error) {
     console.error("Error handling upvote:", error);
@@ -120,25 +146,36 @@ router.post("/comment/user-upvote/:id", async (req, res) => {
     },
   });
   console.log(existingUpvote);
-  if(existingUpvote) {
-    res.json({existed: true});
+  if (existingUpvote) {
+    res.json({ existed: true });
   } else {
-    res.json({existed: false});
+    res.json({ existed: false });
   }
 });
 
-// router.post("comment/upvote/:id/:userid", async (req, res) => {
-//   // #swagger.tags = ['Discussion']
-//   // #swagger.description = 'Post a comment upvote'
-//   const id = parseInt(req.params.id);
-//   const userid = req.params.userid;
-//   await prisma.user_upvote_comment.create({
-//     data: {
-//       comment_id: id,
-//       user_id: userid,
-//     }
-//   })
-//   console.log("create upvote", userid, id);
-// });
+router.patch("/comment/:id/verify", async (req, res) => {
+  // #swagger.tags = ['Discussion']
+  // #swagger.description = 'Verify a comment by id'
+  const id = parseInt(req.params.id);
+  const isAdministrator = req.body.isAdmin;
+
+if (!isAdministrator) {
+    return res.status(401).json({ error: "Unauthorized" });
+  } else {
+    const existingComment = await prisma.comment.findUnique({ 
+      where: { id }
+    });
+
+    if (!existingComment) {
+      return res.status(404).json({ error: "Comment not found" });
+    }
+
+    const comment = await prisma.comment.update({
+      where: { id },
+      data: { verified: !existingComment.verified },
+    }); 
+    res.json(comment);
+  }
+});
 
 export default router;
