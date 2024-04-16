171d8b89496e7d2abd083c42d2f0643f2c90816c
sendist <sendisetiawan10902@gmail.com>
2024-04-16 07:26:38 +0700

    hotfix: delete verified field at comment table

--- init.sql
+++ init.sql
@@ -28,7 +28,6 @@ CREATE TABLE IF NOT EXISTS "comment_reply" (
     "author" TEXT NOT NULL,
     "content" TEXT NOT NULL,
     "anonymous" BOOLEAN NOT NULL DEFAULT FALSE,
-    "verified" BOOLEAN NOT NULL DEFAULT FALSE,
     "created_at" TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
